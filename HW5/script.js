7342605fd8d99c11135a59f531fb75e8e3acda8a
Misha Lubkovskyi <lubkovskiymisha@gmail.com>
2023-06-02 18:39:24 +0300

    hw5

---
+++ HW5/script.js
@@ -0,0 +1,30 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+switch (numOrStr) {
+    case null:
+        console.log('ви відмінили')
+        break;
+    case  '':
+        console.log('Empty String');
+        break;
+    case !isNaN(numOrStr) || numOrStr:
+        console.log(' number is Ba_NaN')
+        break;
+    default:
+        console.log('OK!')
+        break;
+}
+
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+
+// if(numOrStr === null) {
+//     console.log('ви відмінили')
+// } else if( numOrStr.trim() === '' ) {
+//     console.log('Empty String');
+// } else if( isNaN( +numOrStr ) ) {
+//     console.log(' number is Ba_NaN')
+// } else {
+//     console.log('OK!')
+// }
