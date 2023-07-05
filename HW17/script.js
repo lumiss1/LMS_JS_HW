605be0ac91d1dbd01c5e597b4a2732a390cd7c6e
Misha Lubkovskyi <lubkovskiymisha@gmail.com>
2023-07-05 20:37:07 +0300

    hw17 fix

--- HW17/script.js
+++ HW17/script.js
@@ -3,17 +3,18 @@ class Person {
     this.name = name;
     this.age = age;
   }
-  submit() {
+
+  getInfo() {
     console.log(`Ім'я: ${this.name}, Вік: ${this.age}`);
   }
 }
 
 const person1 = new Person("Міша", 19);
-person1.submit();
+person1.getInfo();
 const person2 = new Person("Вова", 21);
-person2.submit();
+person2.getInfo();
 const person3 = new Person("Аліна", 16);
-person3.submit();
+person3.getInfo();
 
 class Car {
   constructor(brand, model, year, license_plate, owner) {
@@ -21,26 +22,33 @@ class Car {
     this.model = model;
     this.year = year;
     this.license_plate = license_plate;
-    this.owner = owner;
+    this.owner = this.getPersonInfo(owner);
   }
-  submit_car() {
-    console.log(
-      `Марка: ${this.brand}, Модель: ${this.model}, Рік: ${
-        this.year
-      }, Номерний знак: ${this.license_plate}, Власник: ${this.person()}`
-    );
-  }
-  person() {
-    if (this.owner.age > 18) {
-      return this.owner.name;
+
+  getPersonInfo(owner) {
+    if (owner.age > 18) {
+      return owner;
     } else {
-      return "Людині немає 18 років";
+      return null;
+    }
+  }
+
+  getInfo() {
+    console.log(
+      `Марка: ${this.brand}, Модель: ${this.model}, Рік: ${this.year}, Номерний знак: ${this.license_plate}`
+    );
+    console.log("Власник:");
+    if (this.owner) {
+      this.owner.getInfo();
+    } else {
+      console.log("Немає 18 років");
     }
   }
 }
+
 const car1 = new Car("Mercedes", "E-class", 2018, "AM 9999 AM", person1);
-car1.submit_car();
+car1.getInfo();
 const car2 = new Car("Audi", "Q8", 2020, "AA 1234 AA", person2);
-car2.submit_car();
-const car3 = new Car("Tayota", "Camry", 2013, "ВХ 3211 ВХ", person3);
-car3.submit_car();
+car2.getInfo();
+const car3 = new Car("Toyota", "Camry", 2013, "ВХ 3211 ВХ", person3);
+car3.getInfo();
